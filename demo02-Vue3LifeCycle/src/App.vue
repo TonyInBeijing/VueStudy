bf7583e7c64022f4b26241460469466f19591905
TonyInBeijing <1300565735@qq.com>
2020-11-09 16:09:25 +0800

    vue3生命周期

---
+++ demo02-Vue3LifeCycle/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  onActivated,
+  onBeforeUpdate,
+  onDeactivated,
+  onMounted,
+  onUnmounted,
+  onUpdated,
+  PropType,
+} from "vue";
+interface Student {
+  name: string;
+  class: string;
+  age: number;
+}
+const Component = defineComponent({
+  setup(props, context) {
+    // 挂载后
+    onMounted(() => {});
+    // 卸载后
+    onUnmounted(() => {});
+    // 更新后
+    onUpdated(() => {});
+    // 即将更新
+    onBeforeUpdate(() => {});
+    // keepalive 组件激活
+    onActivated(() => {});
+    // keepalive 组件非激活
+    onDeactivated(() => {});
+    return {};
+  },
+});
+</script>
