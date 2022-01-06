d97cb8682eae3ad1e2726c2fb07330aef98c3c08
TonyInBeijing <yuehaowei1126@gmail.com>
2022-01-06 10:41:53 +0800

    vue2计算和监听

--- demo05-vue2study/src/components/component1.vue
+++ demo05-vue2study/src/components/component1.vue
@@ -3,10 +3,18 @@
         <h1>{{ msg }}</h1>
         <h2 v-if="showProps">{{ helloMsg }}</h2>
         <h2 v-else>Props 被隐藏了</h2>
-        <h2 v-for="(car,index) in cars" :key="index">{{ car }}</h2>
         <button @click="switchProps">打招呼</button>
-        <button @click="buyCars">买车</button>
-        <button @click="sellCars">卖车</button>
+
+        <h1>{{ carsNum }}</h1>
+        <div>
+            <input v-model="carName" placeholder="请输入您想买的车型" />
+            <button @click="buyCars">我要买车</button>
+        </div>
+
+        <div v-for="(car,index) in cars" :key="car">
+            <h2>车型：{{ car }}</h2>
+            <button @click="sellCars(index)">卖车</button>
+        </div>
     </div>
 </template>
 
@@ -24,18 +32,37 @@ export default {
                 "Benz",
                 "BMW",
                 "Audi"
-            ]
+            ],
+            carName: "",
         };
+    },
+    computed: {
+        carsNum() {
+            return "我还有" + this.cars.length + "辆车可以卖";
+        }
+    },
+    watch: {
+        carName() {
+            console.log(this.carName);
+            // console.log("我上一次买的车是" + oldName);
+            // console.log("我又买了一辆车是" + newName);
+        }
     },
     methods: {
         switchProps() {
             this.showProps = !this.showProps;
         },
         buyCars() {
-            this.cars.push("Toyota");
+            if (this.carName === "") {
+                console.warn("车型不能为空！");
+                return;
+            }
+            else {
+                this.cars.push(this.carName);
+            }
         },
-        sellCars() {
-            this.cars = this.cars.slice(0, this.cars.length - 1);
+        sellCars(index) {
+            this.cars.splice(index, 1)
         }
     },
 }
